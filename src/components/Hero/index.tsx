94688775db939acbcb2f83abba49819613b62b1e
Yuri Alves <yuripiresalves@gmail.com>
2021-06-01 17:00:57 -0300

    add links

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -5,13 +5,21 @@ export function Hero() {
     <div className={styles.container}>
       <div className={styles.content}>
         <div className={styles.info}>
-          <img src="/assets/logo.svg" alt="Logo" />
+          <img src="/assets/logo.svg" alt="Logo" className={styles.logo} />
           <span>Desenvolvedor Web Front-end, ReactJS e Next.js.</span>
           <div className={styles.social}>
-            <img src="/assets/github.svg" alt="Github" />
-            <img src="/assets/linkedin.svg" alt="Linkedin" />
-            <img src="/assets/youtube.svg" alt="YouTube" />
-            <img src="/assets/instagram.svg" alt="Instagram" />
+            <a href="https://github.com/yuripiresalves" target="_blank">
+              <img src="/assets/github.svg" alt="Github" />
+            </a>
+            <a href="https://linkedin.com/in/yuripiresalves" target="_blank">
+              <img src="/assets/linkedin.svg" alt="Linkedin" />
+            </a>
+            <a href="https://youtube.com/c/yuripiresalves" target="_blank">
+              <img src="/assets/youtube.svg" alt="YouTube" />
+            </a>
+            <a href="https://instagram.com/yuripa07" target="_blank">
+              <img src="/assets/instagram.svg" alt="Instagram" />
+            </a>
           </div>
         </div>
         <div className={styles.image} />
