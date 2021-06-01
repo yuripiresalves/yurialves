4b5c5fd15ddc94639753ab9221be59bec6f74539
Yuri Alves <yuripiresalves@gmail.com>
2021-06-01 16:57:47 -0300

    add Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss';
+
+export function Footer() {
+  return (
+    <footer className={styles.container}>
+      <div className={styles.content}>
+        <p>© Desenvolvido com 💜. Yuri Alves.</p>
+      </div>
+    </footer>
+  );
+}
