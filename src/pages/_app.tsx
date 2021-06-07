b9d9d9c42022b1c9d1504f17ac9ac759a2b8f740
Yuri Alves <yuripiresalves@gmail.com>
2021-06-06 21:57:28 -0300

    add react-toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
 
+import 'react-toastify/dist/ReactToastify.css';
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer autoClose={5000} />
+    </>
+  );
 }
 
 export default MyApp;
